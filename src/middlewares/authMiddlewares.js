c77ea218b73903939099c0d9d2f07aefde7052c5
Yosi <Yosi@YosiWebiks.com>
2024-10-07 11:42:05 +0300

    empty middlewares

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,20 @@
+const onlyCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+};
+
+const onlySoldiersAndCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+};
+
+module.exports = {
+  onlyCommanders,
+  onlySoldiersAndCommanders,
+};
