5b96998f64264052813d63c88f1a21688683e2b2
Yosi <Yosi@YosiWebiks.com>
2024-10-07 15:35:39 +0300

    more emotions to the message

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectToMongo = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/green_eye')
-        console.log("successfully connected to MongDB")
+        console.log("successfully connected to MongDB =)")
     } catch (err) {
         console.log(err)
     }
