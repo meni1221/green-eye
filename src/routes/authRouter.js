9380f2b43e8f12bba5fe2c722258d8cabd4e0c74
Yosi <Yosi@YosiWebiks.com>
2024-10-07 11:16:42 +0300

    empty endpoints api docs

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 
+router.post('/login', ()=>{})
+
+router.delete('/logout', ()=>{})
+
 module.exports = router;
 
--- src/routes/greenEyeRouter.js
+++ src/routes/greenEyeRouter.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 
+router.post('/', ()=>{})
+
+router.get('/my', ()=>{})
+
+router.get('/:id', ()=>{})
+
+router.post('/reply', ()=>{})
+
+router.delete('/:id', ()=>{})
+
 module.exports = router;
 
