b92bb2485ca0251e4b4e8468ba67017649051f8f
Yosi <Yosi@YosiWebiks.com>
2024-10-07 10:31:27 +0300

    create and connect empty router files

--- src/app.js
+++ src/app.js
@@ -5,4 +5,10 @@ const app = exp();
 dotenv.config();
 const port = process.env.PORT || 1415;
 
+app.use(exp.json());
+
+app.use("/auth", require("./routes/authRouter"));
+app.use("/user", require("./routes/userRouter"));
+app.use("/greenEye", require("./routes/greenEyeRouter"));
+
 app.listen(port, () => console.log("server up and runing on port " + port));
