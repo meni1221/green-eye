00aee8764419d67bcc0e352f62074cb4a14bf644
Yosi <Yosi@YosiWebiks.com>
2024-10-07 11:28:38 +0300

    empty controllers created

---
+++ src/controllers/authController.js
@@ -0,0 +1,16 @@
+const login = async (req, res) => {
+    try {
+    } catch (err) {}
+  };
+  
+  const logout = async (req, res) => {
+    try {
+    } catch (err) {}
+  };
+  
+  
+  module.exports = {
+    login,
+    logout
+  };
+  
---
+++ src/controllers/userController.js
@@ -0,0 +1,20 @@
+const register = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const getProfile = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const setSettings = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+module.exports = {
+  register,
+  getProfile,
+  setSettings,
+};
