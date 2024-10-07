8e3e6a60e1e9f25575e0dc5262e50c029df38eda
Yosi <Yosi@YosiWebiks.com>
2024-10-07 16:48:45 +0300

    register done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const exp = require("express");
 const dotenv = require("dotenv");
 const { connectToMongo } = require("./config/dbConfig")
+const cookieParser = require("cookie-parser")
 
 const app = exp();
 dotenv.config();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 1415;
 
 
 app.use(exp.json());
+app.use(cookieParser());
 
 app.use("/auth", require("./routes/authRouter"));
 app.use("/user", require("./routes/userRouter"));
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,23 @@
+const { login: loginFromService } = require("../services/authService");
+
 const login = async (req, res) => {
-    try {
-    } catch (err) {}
-  };
-  
-  const logout = async (req, res) => {
-    try {
-    } catch (err) {}
-  };
-  
-  
-  module.exports = {
-    login,
-    logout
-  };
-  
+  try {
+    const token = await loginFromService(req.body);
+    res.cookie("token", token);
+    res.json({
+      msg: `welcome ${req.body.user_name}! so good to see you!!`,
+    });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+module.exports = {
+  login,
+  logout,
+};
